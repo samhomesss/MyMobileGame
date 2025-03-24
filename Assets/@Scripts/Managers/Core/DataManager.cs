db0eab1a0e5150ffbe567d537c4e069e5dd14d56
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2025-03-24 15:58:25 +0900

    Update

---
+++ Assets/@Scripts/Managers/Core/DataManager.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// Json 파일을 불러와서 연결 해주는 역할의 DataManager
+/// </summary>
+public class DataManager
+{
+    
+}
---
+++ Assets/@Scripts/Managers/Core/ResourceManager.cs
@@ -0,0 +1,7 @@
+/// <summary>
+/// 게임에 필요한 리소스를 다 로드하고 메모리에 로드하고 꺼내 쓰는 형태가 이상적인 형태다 
+/// </summary>
+public class ResourceManager
+{
+    
+}
