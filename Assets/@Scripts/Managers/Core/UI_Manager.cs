fa090603af565325501a375b4e279bb2d23584d5
Yoon.07.09 <159544864+samhomesss@users.noreply.github.com>
2025-03-30 16:29:57 +0900

    Update

--- Assets/@Scripts/Managers/Core/UI_Manager.cs
+++ Assets/@Scripts/Managers/Core/UI_Manager.cs
@@ -1,4 +1,5 @@
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -41,8 +42,43 @@ public class UI_Manager
 
         if (cansvasScaler != null)
         {
-           // cansvasScaler.
+            cansvasScaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
+            cansvasScaler.referenceResolution = new Vector2(1080, 1920);
+        }
+        go.GetOrAddComponent<GraphicRaycaster>();
+
+        // sorting이 들어가야 하는 것인가 아닌가 팝업의 경우에는 내가 띄운 순서대로 쫙 떠야 하니까 sorting이 필요한데 
+        if (sort)
+        {
+            canvas.sortingOrder = _order;
+            _order++;
+        }
+        else // SceneUI의 경우 sorting이 필요하지 않기 때문에  이렇게 나눠서 작성
+        {
+            canvas.sortingOrder = sortOrder;
         }
     }
+
+    public T GetSceneUI<T>() where T : UI_Base
+    {
+        return _sceneUI as T;
+    }
+
+    public T MakeWorldSpaceUI<T>(Transform parent = null , string name = null) where T : UI_Base
+    {
+        if (string.IsNullOrEmpty(name))
+            name = typeof(T).Name;
+
+        GameObject go = Managers.Resource.Instantiate($"{name}");
+        if (parent != null)
+            go.transform.SetParent(parent);
+
+        Canvas canvas = go.GetOrAddComponent<Canvas>();
+        canvas.renderMode = RenderMode.WorldSpace;
+        canvas.worldCamera = Camera.main;
+
+        return Util.GetOrAddComponet<T>(go);
+    }
+
 
 }
